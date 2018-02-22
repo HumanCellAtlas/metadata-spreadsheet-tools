4dcf3da88943c5cd51e934561ccdc4a4f8fd6785
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-02-22 17:53:33 +0000

    added example

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# metadata-spreadsheet-tools
+# Metadata Spreadsheet Tools
 Tools for generating metadata spreadsheets
+
+## Commands
+Execute
+```
+serverless invoke local --function generate --path ./example/generate.json
+```
+
+Deploy
+```
+serverless deploy -v
+```
+
+Check logs
+```
+serverless logs --function generate
+```
--- handler.py
+++ handler.py
@@ -1,6 +1,32 @@
-def on_event(event, context):
+import base64
+import json
+
+from json_to_spreadsheet_template import SpreadsheetCreator
+
+
+def generate(event, context):
+    print(json.dumps(event))
+    body = json.loads(event['body'])
+    schema_uri = body['schema_uri']
+    schema_types = body['schema_types']
+    schema_modules = body['schema_modules']
+
+    for index, schema_module in enumerate(schema_modules):
+        schema_modules[index] = schema_uri + schema_module
+
+    filename = body['output_filename']
+    output = "/tmp/" + filename
+    spreadsheet_creator = SpreadsheetCreator()
+    spreadsheet_creator.generateSpreadsheet(schema_uri, schema_types, schema_modules, output)
+    with open(output, "rb") as output_file:
+        encoded_string = base64.b64encode(output_file.read())
     response = {
         "statusCode": 200,
-        "body": {}
+        "headers": {
+            "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "Content-Disposition": "inline; filename=" + filename
+        },
+        "body": str(encoded_string.decode("utf-8")),
+        "isBase64Encoded": True
     }
     return response
